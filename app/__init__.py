b38e2366edcd95348049b3cd470f900d44ed1725
ruganda <mubaruganda@gmail.com>
2018-06-26 21:04:51 +0300

    Register the auth blue print with the application

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,9 @@ def create_app(config_name):
     """
     app = Flask(__name__)  
     app.config.from_object(app_config[config_name])
+    
+    from app.auth.views import auth_blueprint
+    # register_blueprint
+    app.register_blueprint(auth_blueprint)
     return app
+    
