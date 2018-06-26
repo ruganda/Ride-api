351ac58f9338921a94fc2cf58a9ffb03d1038635
ruganda <mubaruganda@gmail.com>
2018-06-26 20:39:27 +0300

    Add user class and database queries

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 import psycopg2
 
 
@@ -10,3 +11,29 @@ class Database:
             "dbname='ride_db' user='postgres' host = 'localhost' password='15december' port='5432'"
         )
         self.cur = self.conn.cursor()
+    
+class User(Database):
+    
+    def __init__(self, user_id=0, name=None, username=None, password=None):
+        Database.__init__(self)
+        self.user_id = user_id
+        self.name = name
+        self.username = username
+        self.password = generate_password_hash(password, method='sha256')      
+        self.rides_given = 0
+        self.rides_taken = 0
+    
+    def get_single_user(self, username):
+            self.cur.execute(
+                "SELECT * FROM users WHERE username = '{}'".format(username))
+            user = self.cur.fetchone()
+            return user
+    
+    def insert_data(self, user):
+        """Adds a new record to the database"""
+        self.cur.execute(
+            "INSERT INTO users (name, username, password, rides_taken, rides_given) VALUES('{}','{}', '{}','{}','{}');"
+                       .format(user.name, user.username, user.password, self.rides_taken, self.rides_given))
+        self.conn.commit()
+    
+    
