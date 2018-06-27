5ca573955b2f2fcf9ff16e487470ec2f6b64ec82
ruganda <mubaruganda@gmail.com>
2018-06-27 11:20:46 +0300

    Create request_app Blueprint and regiser the endpoint

---
+++ app/request/views.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+
+from app.request.api import RequestAPI
+
+request_app = Blueprint('request_app', __name__)
+
+request_view = RequestAPI.as_view('request_api')
+request_app.add_url_rule('/api/v2/rides/<ride_id>/requests',
+                         view_func=request_view, methods=['POST', ])
+
