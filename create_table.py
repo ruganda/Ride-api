fcb4157398496b8a5ef25f75852d5b02481869af
ruganda <mubaruganda@gmail.com>
2018-06-27 11:13:51 +0300

    Edit the requests table

--- create_table.py
+++ create_table.py
@@ -10,7 +10,7 @@ cursor.execute(create_table)
 create_table = "CREATE TABLE IF NOT EXISTS rides (id SERIAL PRIMARY KEY, origin text, destination text, date TIMESTAMP NOT NULL , driver text)"
 cursor.execute(create_table)
 
-create_table = "CREATE TABLE IF NOT EXISTS requests (id SERIAL PRIMARY KEY, ride_id INTEGER, passenger text)"
+create_table = "CREATE TABLE IF NOT EXISTS requests (id SERIAL PRIMARY KEY, ride_id INTEGER, status text, passenger text)"
 cursor.execute(create_table)
 
 connection.commit()
