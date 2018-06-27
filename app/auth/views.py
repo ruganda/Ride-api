fe0eea86c62031d9143f715231cce31aa46cec21
ruganda <mubaruganda@gmail.com>
2018-06-27 03:38:59 +0300

    Add the login url rule

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,15 +1,19 @@
-from app.auth.api import RegistrationView
 from flask import Blueprint
+from app.auth.api import RegistrationView, LoginView
 
 auth_blueprint = Blueprint('auth', __name__)
 
-
 # Define the API resource
 registration_view = RegistrationView.as_view('registration_view')
-
+login_view = LoginView.as_view('login_view')
 
 # Add the url rule for registering a user
 auth_blueprint.add_url_rule(
     '/api/v2/auth/register',
     view_func=registration_view,
     methods=['POST'])
+auth_blueprint.add_url_rule(
+    '/api/v2/auth/login',
+    view_func=login_view,
+    methods=['POST']
+)
