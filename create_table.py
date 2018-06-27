abb1560e84efeed8a3c5cf9e99a21561cd602ef7
ruganda <mubaruganda@gmail.com>
2018-06-27 04:47:08 +0300

    Add ride table and request table

--- create_table.py
+++ create_table.py
@@ -6,3 +6,12 @@ cursor = connection.cursor()
 
 create_table = "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name text, username text, password text, rides_taken INTEGER, rides_given INTEGER )"
 cursor.execute(create_table)
+
+create_table = "CREATE TABLE IF NOT EXISTS rides (id SERIAL PRIMARY KEY, origin text, destination text, date TIMESTAMP NOT NULL , driver text)"
+cursor.execute(create_table)
+
+create_table = "CREATE TABLE IF NOT EXISTS requests (id SERIAL PRIMARY KEY, ride_id INTEGER, passenger text)"
+cursor.execute(create_table)
+
+connection.commit()
+connection.close()
