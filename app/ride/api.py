b5461c42b352bbf6b9f7b96574245d52647ad2d2
ruganda <mubaruganda@gmail.com>
2018-06-28 06:41:40 +0300

    validate ride offers and date

--- app/ride/api.py
+++ app/ride/api.py
@@ -2,7 +2,7 @@ from flask.views import MethodView
 from flask import jsonify, request, abort, make_response
 from app.models import Ride
 from app.auth.decoractor import token_required
-
+from app.validate import validate_date, validate_ride
 
 class RideAPI(MethodView):
     decorators = [token_required]
@@ -13,31 +13,36 @@ class RideAPI(MethodView):
         origin = data['origin']
         destination = data['destination']
         date = data['date']
-        ride = Ride(origin=origin, destination=destination, date=date)
-        try:
-            all_rides = ride.fetch_all()
-            for this_ride in all_rides:
-                if this_ride['origin'] == ride.origin and \
-                        this_ride['destination'] == ride.destination\
-                        and this_ride['date'] == ride.date and \
-                        this_ride['driver'] == current_user[2]:
-                    response = {
-                        'message': 'This ride already exists.',
-                    }
-                    return make_response(jsonify(response)), 409
-            driver = current_user[2]
-            ride.insert(driver)
+        if validate_date(date) != 'valid':
+            return jsonify({'message':validate_date(date)}), 406
+        elif validate_ride(data) == 'valid':
+            ride = Ride(origin=origin, destination=destination, date=date)
 
-            response = {
-                'message': 'You offered a ride successfully.',
-            }
-            return make_response(jsonify(response)), 201
+            try:
+                all_rides = ride.fetch_all()
+                for this_ride in all_rides:
+                    if this_ride['origin'] == ride.origin and \
+                            this_ride['destination'] == ride.destination\
+                            and this_ride['date'] == ride.date and \
+                            this_ride['driver'] == current_user[2]:
+                        response = {
+                            'message': 'This ride already exists.',
+                        }
+                        return make_response(jsonify(response)), 409
+                driver = current_user[2]
+                ride.insert(driver)
 
-        except Exception as e:
-            response = {
-                'message': str(e)
-            }
-            return make_response(jsonify(response)), 500
+                response = {
+                    'message': 'You offered a ride successfully.',
+                }
+                return make_response(jsonify(response)), 201
+
+            except Exception as e:
+                response = {
+                    'message': str(e)
+                }
+                return make_response(jsonify(response)), 500
+        return jsonify({'message':validate_ride(data)}), 406
 
     def get(self, current_user, r_id):
         """Method for  get requests"""
