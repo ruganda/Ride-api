6e7c41c5a06d2ade1a515a1665b6299e08b8e569
ruganda <mubaruganda@gmail.com>
2018-06-28 20:04:44 +0300

    Fix adding of duplicate requests

--- app/request/api.py
+++ app/request/api.py
@@ -10,20 +10,22 @@ from app.models import Request
 class RequestAPI(MethodView):
     """This class-based view for requesting a ride."""
     decorators = [token_required]
-    def post(self,current_user, ride_id):
+
+    def post(self, current_user, ride_id):
         if ride_id:
             try:
-                request = Request()
+                request = Request(ride_id=ride_id)
                 passenger = current_user[2]
-                all_reqs = request.fetch_all()
-                for req in  all_reqs:
-                    req= {"Id":req['Id'],"ride":req['ride'],"passenger":req['passenger']}
+                all_reqs = request.find_by_id(ride_id)
+                for req in all_reqs:
+                    req = {"Id": req['Id'], "ride_id": req['ride_id'],
+                           "status": req['status'], "passenger": req['passenger']}
                     if req in all_reqs:
-                        return jsonify({'msg': 'You already requested to join this ride' }), 409
-                    
-                request.insert(ride_id, passenger)  
-                return jsonify({'msg': 'A request to join this ride has been sent' }), 201 
-            
+                        return jsonify({'msg': 'You already requested to join this ride'}), 409
+
+                request.insert(ride_id, passenger)
+                return jsonify({'msg': 'A request to join this ride has been sent'}), 201
+
             except Exception as e:
                 response = {
                     'message': str(e)
@@ -31,10 +33,10 @@ class RequestAPI(MethodView):
                 return make_response(jsonify(response)), 500
 
     def get(self, current_user, ride_id):
-        '''Gets all requests''' 
+        '''Gets all requests'''
         try:
-            request = Request()
-            requests = request.fetch_all()
+            request = Request(ride_id=ride_id)
+            requests = request.find_by_id(ride_id)
             if requests == []:
                 return jsonify({"msg": "You haven't recieved any ride requests yet"}), 200
             return jsonify(requests), 200
@@ -43,16 +45,16 @@ class RequestAPI(MethodView):
                 'message': str(e)
             }
             return make_response(jsonify(response)), 500
-   
-    def put(self,current_user, ride_id, request_id):
+
+    def put(self, current_user, ride_id, request_id):
         """Accept or reject a ride request"""
         data = request.get_json()
-        if data['status']=='accepted' or data['status'] =='rejected':
+        if data['status'] == 'accepted' or data['status'] == 'rejected':
             try:
-                req=Request(id=request_id,ride_id = ride_id)
-                req.update_request(request_id,data)
+                req = Request(id=request_id, ride_id=ride_id)
+                req.update_request(request_id, data)
                 response = {
-                'message':'you have {} this ride request'.format(data['status'])
+                    'message': 'you have {} this ride request'.format(data['status'])
                 }
                 return make_response(jsonify(response)), 200
             except Exception as e:
@@ -61,6 +63,6 @@ class RequestAPI(MethodView):
                 }
                 return make_response(jsonify(response)), 500
         response = {
-            'message':'The status can only be in 3 states, requested, accepted and rejected'
+            'message': 'The status can only be in 3 states, requested, accepted and rejected'
         }
         return make_response(jsonify(response)), 406
