1b2c1c1b02ea77fc27bf258bfd99b69cfc20e0e1
ruganda <mubaruganda@gmail.com>
2018-06-30 08:24:15 +0300

    Correct error in creatin tables

--- create_table.py
+++ create_table.py
@@ -5,17 +5,17 @@ connection = psycopg2.connect(
      password='15december' port ='5432'")
 cursor = connection.cursor()
 
-create_table = "CREATE TABLE IF NOT EXISTS users
-(id SERIAL PRIMARY KEY, name text, username text, password text,
+create_table = "CREATE TABLE IF NOT EXISTS users\
+(id SERIAL PRIMARY KEY, name text, username text, password text,\
  rides_taken INTEGER, rides_given INTEGER)"
 cursor.execute(create_table)
 
-create_table = "CREATE TABLE IF NOT EXISTS rides
-(id SERIAL PRIMARY KEY, origin text, destination
+create_table = "CREATE TABLE IF NOT EXISTS rides\
+(id SERIAL PRIMARY KEY, origin text, destination\
  text, date TIMESTAMP NOT NULL, driver text)"
 cursor.execute(create_table)
 
-create_table = "CREATE TABLE IF NOT EXISTS requests
+create_table = "CREATE TABLE IF NOT EXISTS requests\
 (id SERIAL PRIMARY KEY, ride_id INTEGER, status text, passenger text)"
 cursor.execute(create_table)
 
