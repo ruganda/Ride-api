17da09aa0b0034e9138ed2faf894a2584c7cc95e
ruganda <mubaruganda@gmail.com>
2018-06-30 07:09:11 +0300

    Add fuctinality for to driver to respond to only rides he created

--- app/request/api.py
+++ app/request/api.py
@@ -1,7 +1,7 @@
 from flask import jsonify, make_response, request
 from flask.views import MethodView
 from app.auth.decoractor import token_required
-from app.models import Request
+from app.models import Request, Ride
 
 
 class RequestAPI(MethodView):
@@ -16,12 +16,15 @@ class RequestAPI(MethodView):
                 all_reqs = request.find_by_id(ride_id)
                 for req in all_reqs:
                     req = {"Id": req['Id'], "ride_id": req['ride_id'],
-                           "status": req['status'], "passenger": req['passenger']}
+                           "status": req['status'],
+                           "passenger": req['passenger']}
                     if req in all_reqs:
-                        return jsonify({'msg': 'You already requested to join this ride'}), 409
+                        return jsonify({'msg': 'You already requested\
+                         to join this ride'}), 409
 
                 request.insert(ride_id, passenger)
-                return jsonify({'msg': 'A request to join this ride has been sent'}), 201
+                return jsonify({'msg': 'A request to join this ride\
+                 has been sent'}), 201
 
             except Exception as e:
                 response = {
@@ -35,7 +38,8 @@ class RequestAPI(MethodView):
             request = Request(ride_id=ride_id)
             requests = request.find_by_id(ride_id)
             if requests == []:
-                return jsonify({"msg": "You haven't recieved any ride requests yet"}), 200
+                return jsonify({"msg": "You haven't recieved any ride\
+                 requests yet"}), 200
             return jsonify(requests), 200
         except Exception as e:
             response = {
@@ -45,21 +49,34 @@ class RequestAPI(MethodView):
 
     def put(self, current_user, ride_id, request_id):
         """Accept or reject a ride request"""
-        data = request.get_json()
-        if data['status'] == 'accepted' or data['status'] == 'rejected':
-            try:
-                req = Request(id=request_id, ride_id=ride_id)
-                req.update_request(request_id, data)
-                response = {
-                    'message': 'you have {} this ride request'.format(data['status'])
-                }
-                return make_response(jsonify(response)), 200
-            except Exception as e:
-                response = {
-                    'message': str(e)
-                }
-                return make_response(jsonify(response)), 500
+        # first check if the ride was created by the logged in driver
+        ride = Ride(id=ride_id)
+        the_ride = ride.find_by_id(ride_id)
+        print(the_ride["driver"])
+        print(current_user[2])
+        if the_ride['driver'] == current_user[2]:
+            data = request.get_json()
+            if data['status'] == 'accepted' or data['status'] == 'rejected':
+                try:
+                    req = Request(id=request_id, ride_id=ride_id)
+                    req.update_request(request_id, data)
+                    response = {
+                        'message': 'you have {} this ride request'
+                        .format(data['status'])
+                    }
+                    return make_response(jsonify(response)), 200
+                except Exception as e:
+                    response = {
+                        'message': str(e)
+                    }
+                    return make_response(jsonify(response)), 500
+            response = {
+                'message': 'The status can only be in 3 states,\
+                requested, accepted and rejected'
+            }
+            return make_response(jsonify(response)), 406
         response = {
-            'message': 'The status can only be in 3 states, requested, accepted and rejected'
+            'message': 'Forbidden access! You can only repond \
+            to rides you created'
         }
-        return make_response(jsonify(response)), 406
+        return jsonify(response)
