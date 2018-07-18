0cb37e43b8cae79a6c3231ef59bc11d517b0f348
ruganda <mubaruganda@gmail.com>
2018-07-18 19:46:38 +0300

    Add flask CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 """Creates module is the main application factory"""
 from flask import Flask
+from flask_cors import CORS
 from config import app_config
 from app.database import Database
 from app.error_handler import *
@@ -10,14 +11,19 @@ def create_app(config_name):
         with the application
     """
     app = Flask(__name__)
+    CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
+
     app.config.from_object(app_config[config_name])
     from app.request.views import REQUEST_APP
     from app.ride.views import RIDE_APP
     from app.auth.views import AUTH_BLUEPRINT
+
     # register_blueprint
     app.register_blueprint(AUTH_BLUEPRINT)
     app.register_blueprint(RIDE_APP)
     app.register_blueprint(REQUEST_APP)
+
     # register error handlers
     app.register_error_handler(404, not_found)
     app.register_error_handler(400, bad_request)
