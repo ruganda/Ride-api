3a7f18932a559f4332cc76cf312563f67f330d58
ruganda <mubaruganda@gmail.com>
2018-07-21 01:39:13 +0300

    Change database name

--- manage.py
+++ manage.py
@@ -7,6 +7,6 @@ app = create_app('PRODUCTION')
 db = Database('postgres://xmgdymngkwtqzl:29fa7e2b61e50a08cd256f312cbc59af3a972126f772016ded78355e87a121aa@ec2-50-19-86-139.compute-1.amazonaws.com:5432/d71q4cvgvtvqck')
 
 if __name__ == '__main__':
-    db.create_database('ride_db')
+    db.create_database('d71q4cvgvtvqck')
     db.create_tables()
     app.run(debug=True)
