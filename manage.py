a985907c90bf170b6847ac73165ba8552ba36c89
ruganda <mubaruganda@gmail.com>
2018-07-21 02:52:59 +0300

    Change database name

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app import create_app
 from app.database import Database
 
 app = create_app('PRODUCTION')
-db = Database('postgresql://postgres:15december@localhost:5432/heroku_db')
+db = Database('postgresql://postgres:15december@localhost:5432/ride_db')
 
 if __name__ == '__main__':
     db.create_database('heroku_db')
