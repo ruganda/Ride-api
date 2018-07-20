4e4ae4337e3609623ec530afa09ea975bd548c32
ruganda <mubaruganda@gmail.com>
2018-07-21 01:33:55 +0300

    Add heroku database

--- manage.py
+++ manage.py
@@ -3,8 +3,8 @@ import psycopg2
 from app import create_app
 from app.database import Database
 
-app = create_app('DEFAULT')
-db = Database('postgresql://postgres:15december@localhost:5432/ride_db')
+app = create_app('PRODUCTION')
+db = Database('postgres://xmgdymngkwtqzl:29fa7e2b61e50a08cd256f312cbc59af3a972126f772016ded78355e87a121aa@ec2-50-19-86-139.compute-1.amazonaws.com:5432/d71q4cvgvtvqck')
 
 if __name__ == '__main__':
     db.create_database('ride_db')
