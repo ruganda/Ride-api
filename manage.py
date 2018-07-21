c6b85baf63e6d2ff7049a412959068fb0122c648
ruganda <mubaruganda@gmail.com>
2018-07-21 04:36:24 +0300

    change run command

--- manage.py
+++ manage.py
@@ -9,5 +9,5 @@ db = Database('postgres://akcolxjufhesko:d01b99b7009c67760234fb1bcb4229e5566490f
 if __name__ == '__main__':
     # db.create_database('ride_db')
     db.create_tables()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='127.0.0.1', port=port)
+    # port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True)
