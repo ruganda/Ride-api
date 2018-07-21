48cd48fb867798403f118999f2e9144e1d59bcfd
ruganda <mubaruganda@gmail.com>
2018-07-21 03:00:46 +0300

    Correct database name

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ app = create_app('PRODUCTION')
 db = Database('postgresql://postgres:15december@localhost:5432/ride_db')
 
 if __name__ == '__main__':
-    db.create_database('heroku_db')
+    db.create_database('ride_db')
     db.create_tables()
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, port=port)
