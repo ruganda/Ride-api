2c9cf84688331a93c01f2a8eb4ee4dcbb7eaff3b
ruganda <mubaruganda@gmail.com>
2018-07-21 03:00:11 +0300

    Add port to heroku

--- manage.py
+++ manage.py
@@ -9,4 +9,5 @@ db = Database('postgresql://postgres:15december@localhost:5432/ride_db')
 if __name__ == '__main__':
     db.create_database('heroku_db')
     db.create_tables()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port)
